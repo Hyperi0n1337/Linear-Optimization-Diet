2f3e8f3655ac6d973a60fb7dec08d314b45c315e
user <user>
2024-06-15 16:43:18 -0400

    fix json clobbering, bad index

--- athlete_manager.py
+++ athlete_manager.py
@@ -14,13 +14,15 @@ class Requirements(BaseModel):
 
 # If the Requirements are optional, the current athlete dict -> Athlete
 # parse will still work, and Requirements can be constructed separately
+# TODO: Fields should have case standardized, there's a way to get around
+# this but it's very ugly and I don't want to do it right now
 class Athlete(BaseModel):
     Name: Optional[str]
     Weight_lbs: Optional[float]
-    BF_Percent: Optional[float]
-    Caloric_Multiplier: Optional[float]
-    Extra_Caloric_Multiplier: Optional[float]
-    extra_PSMF_multiplier: Optional[float]
+    Bf_percent: Optional[float]
+    Caloric_multiplier: Optional[float]
+    Extra_caloric_multiplier: Optional[float]
+    Extra_psmf_multiplier: Optional[float]
     # requirements: Optional[Requirements]
 
 # TODO: Pass this, and create_athlete(), an athlete object and a valid filepath:
@@ -30,13 +32,13 @@ class Athlete(BaseModel):
 # attrs = ['Markos', 194, 0.17, 15.1, 1, 1] ## NOTE: you could use a dict if you want to see labels
 # athlete = Athlete(*attrs)
 #
-# csv_path = whatever is the right way to do this
+# csv_path = whatever is the right way to do this that guarantees it exists
 #
-# create_athlete(athlete, csv_path)
+# update_athlete(athlete, csv_path)
 #
 # Then the checks can be removed from these fns; pydantic will error if you create
 # an athlete with invalid attrs immediately. You could also just do a bare
-# get_athlete("markos"), since there's no need to create your own entry each time
+# get_athlete("markos"), since there's no need to create your own entry each time.
 def update_athlete(
     name: str,
     weight_lbs: Optional[float],
@@ -47,27 +49,38 @@ def update_athlete(
     file_path: str = './athletes.json'
 ) -> Optional[Athlete]:
 
-    try:
-        try:
-            with open(file_path, 'r') as file:
-                athletes = json.load(file)
-        except json.JSONDecodeError as e:
-            print(f"Couldn't decode:\n {e}")
-    except FileNotFoundError as e:
-        print(f"No athlete files exist here: {e}")
+    # this is less good than I originally thought, but can be
+    # resurrected when this fn is given a valid filepath
+#    try:
+#        try:
+#            with open(file_path, 'r') as file:
+#                athletes = json.load(file)
+#        except json.JSONDecodeError as e:
+#            print(f"Couldn't decode:\n {e}")
+#    except FileNotFoundError as e:
+#        print(f"No athlete files exist here: {e}")
+
+    supplied_values = {k.capitalize():v for k,v in locals().items() if k != "file_path"}
+
+    # Check validation before continuing
+    new_athlete = Athlete(**supplied_values)
+
+    # Load existing athletes or create file if not present
+    # TODO: Validation should be elsewhere
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            athletes = json.load(file)
+    else:
+        raise FileNotFoundError
 
     # Err if not present
     assert name in athletes, f"{name} has no entries, I only found:\n{athletes.keys()}"
 
-    # NOTE: Code golf idiocy, does this even work?
-    # Take passed in vars, except the last, and make a dict out of them
-    supplied_values = {k:v for k,v in locals()[:-1].items() if v is not None}
-
     # Make a new Athlete out of a new dict, made by right-hand-side preferred merge
-    updated_athlete = Athlete({**athletes[name], **supplied_values})
+    updated_athlete = Athlete(**{**athletes[name], **supplied_values})
 
     # Mutate athletes dict at key Athlete.Name
-    athletes[update_athlete.Name] = updated_athlete.dict()
+    athletes[updated_athlete.Name] = updated_athlete.dict()
 
     # Save back to the JSON file
     try:
@@ -87,26 +100,23 @@ def create_athlete(
     file_path: str = './athletes.json'
 ) -> None:
     # Ensure the directory exists
-    # NOTE: I think this will kill the file if it already exists
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    supplied_values = {k:v for k,v in locals()[:-1].items() if v is not None}
+    supplied_values = {k.capitalize():v for k,v in locals().items() if k != "file_path"}
+
+    # Load existing athletes or create file if not present
+    # TODO: Validation should be elsewhere
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            athletes = json.load(file)
+    else:
+        athletes = {}
+
+    assert name not in athletes.keys(), f"{name} already exists: {Athlete(**athletes[name])}"
 
     new_athlete = Athlete(**supplied_values)
 
-    # Load existing athletes
-    try:
-        try:
-            with open(file_path, 'r') as file:
-                athletes = json.load(file)
-        except json.JSONDecodeError as e:
-            print(f"Couldn't decode:\n {e}")
-    except FileNotFoundError as e:
-        print(f"No athletes exist here:\n {e}")
-
-    assert new_athlete.Name not in athletes.keys(), f"{new_athlete.Name} already exists: {Athlete(**athletes.name)}"
-
-    athletes = athletes | new_athlete.dict()
+    athletes = athletes | {new_athlete.Name: new_athlete.dict()}
 
     try:
         with open(file_path, 'w') as file:
